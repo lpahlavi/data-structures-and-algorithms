beef081f2774c67c563bce7340c1558d62cde503
Louis Pahlavi <louis.pahlavi@dfinity.org>
2024-12-18 20:50:53 +0100

    Fix bug in union find algorithm

--- union_find.py
+++ union_find.py
@@ -3,10 +3,10 @@
 # to any one of their respective elements.
 # The use of path compression and union-by rank gives a worst case runtime of α(n)
 # for both the `find` and `union` methods, where α is the inverse Ackermann function.
-class DisjointSet:
+class UnionFind:
     def __init__(self, num_nodes: int):
         self.parents = [i for i in range(num_nodes)]
-        self.ranks = [0 for _ in range(num_nodes)]
+        self.sizes = [0 for _ in range(num_nodes)]
 
     # Find the index of the parent of `node`
     def find(self, node: int) -> int:
@@ -18,17 +18,16 @@ class DisjointSet:
     # Merge the sets containing nodes `node1` and `node2`. Returns `True` is a merge
     # occurred, and `False` if the two nodes were already in the same set.
     def union(self, node1: int, node2: int) -> bool:
-        parent1 = self.parents[node1]
-        parent2 = self.parents[node2]
+        parent1 = self.find(node1)
+        parent2 = self.find(node2)
 
         if parent1 == parent2:
             return False
 
-        if self.ranks[parent1] == self.ranks[parent2]:
+        if self.sizes[parent1] > self.sizes[parent2]:
             self.parents[parent2] = parent1
-            self.ranks[parent1] += 1
-        elif self.ranks[parent1] > self.ranks[parent2]:
-            self.parents[parent2] = parent1
+            self.sizes[parent1] += self.sizes[parent2]
         else:
             self.parents[parent1] = parent2
-        return True 
+            self.sizes[parent2] += self.sizes[parent1]
+        return True
